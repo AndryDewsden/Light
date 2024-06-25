0c1168453e7cb778ce6a59c671a28cace15cd6f5
10210795 <10210795@A312PC13>
2024-06-25 12:17:32 +0300

    1505

--- app/src/main/java/com/example/light/MainActivity.kt
+++ app/src/main/java/com/example/light/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.example.light
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.Image
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+
 
 class MainActivity : AppCompatActivity() {
+    //override fun onCreate(savedInstanceState: Bundle?) {
+        //super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+    //}
+
+    fun onMyButtonClick(view: View?)
+    {
+
+        //Toast.makeText(this, "Зачем вы нажали?", Toast.LENGTH_SHORT).show()
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val click: Button = findViewById(R.id.click)
+        val redLight: ImageView = findViewById(R.id.RedLight)
+        val yellowLight: ImageView = findViewById(R.id.YellowLight)
+        val greenLight: ImageView = findViewById(R.id.GreenLight)
+
+        var a = true
+
+        click.setOnClickListener()
+        {
+            if(a == true)
+            {
+                Toast.makeText(this, "Красный", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Жёлтый", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Зелёный", Toast.LENGTH_SHORT).show()
+                a = false
+            }
+
+            else
+            {
+                Toast.makeText(this, "Зелёный", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Жёлтый", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Красный", Toast.LENGTH_SHORT).show()
+                a = true
+            }
+
+            redLight.setColorFilter(ContextCompat.getColor(applicationContext,R.color.black))
+            yellowLight.setColorFilter(ContextCompat.getColor(applicationContext,R.color.black))
+            greenLight.setColorFilter(ContextCompat.getColor(applicationContext,R.color.black))
+        }
     }
+
 }
